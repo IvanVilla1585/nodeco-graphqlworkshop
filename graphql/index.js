65e2c663cc4d936ee5d8c034187a5dd1f4698c46
Ivan Dario Villa Ramirez <trezeguet55@gmail.com>
2018-11-21 23:48:04 -0500

    <graphql index.js>
    graphql schema configuration

---
+++ graphql/index.js
@@ -0,0 +1,28 @@
+
+const merge = require('lodash.merge')
+const { makeExecutableSchema } = require('apollo-server')
+
+const schemaDirectives = require('./directives')
+const studentSchema =  require('./student/schema')
+const studentResolver =  require('./student/resolver')
+const courseSchema =  require('./course/schema')
+const courseResolver =  require('./course/resolver')
+
+const rootSchema = [
+  studentSchema,
+  courseSchema
+]
+
+const resolvers = merge([
+  studentResolver,
+  courseResolver
+])
+
+const schema = makeExecutableSchema({
+  resolvers,
+  schemaDirectives,
+  typeDefs: rootSchema
+})
+
+module.exports = schema
+
