d05daad6ebdab3bac8d93922953671e0a783194b
Ivan Dario Villa Ramirez <trezeguet55@gmail.com>
2018-11-21 23:46:31 -0500

    <graphql student>
    create graphql schema and resolver

---
+++ graphql/student/resolver.js
@@ -0,0 +1,39 @@
+
+// const mongo = require('./db')
+const pubsub = require('../pubsub')
+
+const resolver = {
+  Query: {
+    async students(root, args, { dataSources: { mongoDB }}) {
+      let results = []
+      try {
+        results = await mongoDB.get('students', {})
+      } catch (err) {
+        throw err
+      }
+      return results
+    }
+  },
+  Mutation: {
+    async studentCreate(root, args, { dataSources: { mongoDB }}) {
+      let result = {}
+      try {
+        result = await mongoDB.create('students', args.input)
+        pubsub.asyncIterator('studentCreate', result)
+      } catch (err) {
+        throw err
+      }
+      return result
+    }
+  },
+  Subscription: {
+    studentCreate: {
+      subscribe: () => {
+        console.log('subscription')
+        return pubsub.asyncIterator('studentCreate')
+      }
+    }
+  }
+}
+
+ module.exports = resolver
---
+++ graphql/student/schema.js
@@ -0,0 +1,50 @@
+
+const schema = `
+
+  directive @upper on FIELD_DEFINITION
+
+  enum STATUS {
+    ACTIVE,
+    INACTIVE
+  }
+
+  type Student {
+    "esto es un id"
+    id: ID,
+    name: String @deprecated(reason: "test"),
+    lastName: String @upper,
+    email: String,
+    average: Float,
+    status: STATUS
+  }
+
+  input StudentCreateInput {
+    name: String!,
+    lastName: String,
+    email: String!,
+    average: Float
+  }
+
+  input StudentUpdateInput {
+    name: String,
+    lastName: String,
+    average: Float
+  }
+
+  type Query {
+    students: [Student]
+    studentById(id: ID!): Student
+  }
+
+  type Mutation {
+    studentCreate(input: StudentCreateInput): Student
+    studentUpdate(id: ID!, input: StudentUpdateInput): Student
+    studentDelete(id: ID!): Student
+  }
+
+  type Subscription {
+    studentCreate(input: StudentCreateInput): Student
+  }
+`
+
+module.exports = schema
