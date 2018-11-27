c6f3324f20cc836b9823b36b84f90d22dc77d042
Ivan Dario Villa Ramirez <trezeguet55@gmail.com>
2018-11-27 13:44:28 -0500

    <graphql student resolver.js>
    complete CRUD operations

--- graphql/student/resolver.js
+++ graphql/student/resolver.js
@@ -3,23 +3,55 @@ const pubsub = require('../pubsub')
 
 const resolver = {
   Query: {
-    async students(root, args, { dataSources: { mongoDB }}) {
+    async students(root, args, { dataSources: { studentConnector }}) {
       let results = []
       try {
-        results = await mongoDB.get('students', {})
+        results = await studentConnector.find()
       } catch (err) {
+        console.error(err)
+        throw err
+      }
+      return results
+    },
+    async studentById(root, args, { dataSources: { studentConnector }}) {
+      let results = []
+      try {
+        results = await studentConnector.findById(args.id)
+      } catch (err) {
+        console.error(err)
         throw err
       }
       return results
     }
   },
   Mutation: {
-    async studentCreate(root, args, { dataSources: { mongoDB }}) {
+    async studentCreate(root, args, { dataSources: { studentConnector }}) {
       let result = {}
       try {
-        result = await mongoDB.create('students', args.input)
-        pubsub.asyncIterator('STUDENT_CREATED', result)
+        result = await studentConnector.create({ ...args.input })
+        pubsub.publish('STUDENT_CREATED', { studentCreate: result })
       } catch (err) {
+        console.error(err)
+        throw err
+      }
+      return result
+    },
+    async studentUpdate(root, args, { dataSources: { studentConnector }}) {
+      let result = {}
+      try {
+        result = await studentConnector.update(args.id, { ...args.input })
+      } catch (err) {
+        console.error(err)
+        throw err
+      }
+      return result
+    },
+    async studentDelete(root, args, { dataSources: { studentConnector }}) {
+      let result = {}
+      try {
+        result = await studentConnector.remove(args.id)
+      } catch (err) {
+        console.error(err)
         throw err
       }
       return result
